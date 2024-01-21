889aa55394b990a0691e6843af4d69b49b21daa8
yogeshprakhar <yogeshprakharpal200027@gmail.com>
2024-01-21 19:40:39 +0530

    add new routes and controllers

--- src/app.js
+++ src/app.js
@@ -21,10 +21,29 @@ app.use(cookieParser())
 
 //routes
 import userRouter from "./routes/user.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
+
 
 //routes declaration
-app.use("/users",userRouter)
-// https://localhost:8000/api/v1/users/register
+app.use("/healthcheck", healthcheckRouter);
+app.use("/users", userRouter);
+app.use("/tweets", tweetRouter);
+app.use("/subscriptions", subscriptionRouter);
+app.use("/videos", videoRouter);
+app.use("/comments", commentRouter);
+app.use("/likes", likeRouter);
+app.use("/playlist", playlistRouter);
+app.use("/dashboard", dashboardRouter);
+
+
+// https://localhost:8000/users/register
 
 
 export {app}
